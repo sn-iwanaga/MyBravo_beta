5cc45102c56b54b49b2065a2a4f07327d3757790
sn-iwanaga <sn-iwanaga@unext-hd.jp>
2025-05-09 14:02:42 +0900

    [Add] 履歴モデルの実装（機能は未完了）

--- histories/models.py
+++ histories/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.conf import settings
+from actions.models import Action
+from rewards.models import Reward
 
-# Create your models here.
+class History(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    action = models.ForeignKey(Action, on_delete=models.SET_NULL, null=True, blank=True)
+    reward = models.ForeignKey(Reward, on_delete=models.SET_NULL, null=True, blank=True)
+    date = models.DateField(auto_now_add=True)
+    point_change = models.IntegerField()
+    current_total_points = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        if self.action:
+            return f"{self.user.username} - {self.action.action_name} - {self.date}"
+        elif self.reward:
+            return f"{self.user.username} - {self.reward.reward_name} - {self.date}"
+        else:
+            return f"{self.user.username} - {self.date}"

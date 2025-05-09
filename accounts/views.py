83f904ccd8cbda7c2445df51756837f122b024f7
sn-iwanaga <sn-iwanaga@unext-hd.jp>
2025-05-09 11:55:28 +0900

    [Feat] カスタムユーザモデルの実装

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import AuthenticationForm
+from .forms import CustomUserCreationForm
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+def signup(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('mypage')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'accounts/signup.html', {'form': form})
+
+def signin(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('mypage') 
+    else:
+        form = AuthenticationForm()
+    return render(request, 'accounts/signin.html', {'form': form})
+
+@login_required
+def mypage(request):
+    return render(request, 'accounts/mypage.html')

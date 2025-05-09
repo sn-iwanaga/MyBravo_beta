ce732986212186df273bc6d965be3cd00f039c37
sn-iwanaga <sn-iwanaga@unext-hd.jp>
2025-05-09 15:50:53 +0900

    [Fix]  page structure

--- histories/urls.py
+++ histories/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('record_action/<int:action_pk>/', views.record_action, name='record_action'),
     path('exchange_reward/<int:reward_pk>/', views.exchange_reward, name='exchange_reward'),
+    path('list/', views.history_list, name='history_list'),
 ]
